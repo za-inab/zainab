aff32a99256a5d6d0ec2c95e4ab82982d4ec5a79
Zainab Asif <zainabasifch22@gmail.com>
2025-09-17 22:26:00 +0500

    fixing project cards

--- src/components/ProjectComponent.tsx
+++ src/components/ProjectComponent.tsx
@@ -13,26 +13,37 @@ export interface Project {
 
 const ProjectCard: React.FC<{ project: Project }> = ({ project }) => {
   return (
-    <div className="rounded-xl border border-gray-700 bg-gray-900/40 overflow-hidden group hover:shadow-xl transition-all duration-300">
-      <div className="relative w-full h-48 overflow-hidden">
+    <div className="rounded-lg border border-gray-700 bg-gray-900/40 overflow-hidden group hover:shadow-xl transition-all duration-300 p-2.5">
+      <div className="relative w-full h-48 overflow-hidden p-2">
         <Image
           src={project.photo}
           alt={project.name}
           width={500}
           height={300}
-          className="object-cover w-full h-full transform group-hover:scale-105 transition-transform duration-300"
+          className="object-cover w-full h-full rounded-lg transform group-hover:scale-105 transition-transform duration-300"
         />
       </div>
-      <div className="p-4">
-        <h3 className="text-lg font-semibold text-white">{project.name}</h3>
-        <p className="text-gray-400 text-sm mt-2">{project.description}</p>
-        <Link
-          href={project.liveLink}
-          target="_blank"
-          className="inline-block mt-3 text-emerald-400 text-sm font-medium hover:underline"
-        >
-          View Project →
-        </Link>
+      <div className="p-3">
+        <div className="h-[40%]">
+          <div className="flex flex-row gap-1">
+            <h3 className="text-lg whitespace-nowrap font-semibold text-white">
+              {project.name}
+            </h3>
+            <div className="mx-1 h-0.5 bg-emerald-400 w-full self-center items-center justify-center"></div>
+          </div>
+          <p className="text-gray-400 text-sm mt-2 text-ellipsis">
+            {project.description}
+          </p>
+        </div>
+        <div className="">
+          <Link
+            href={project.liveLink}
+            target="_blank"
+            className="inline-block mt-3 text-emerald-400 text-sm font-medium hover:underline"
+          >
+            View Project →
+          </Link>
+        </div>
       </div>
     </div>
   );
