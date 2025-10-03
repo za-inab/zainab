603430c5364d9df61ba7d9ba810ce343f6ddd4de
Zainab Asif <zainabasifch22@gmail.com>
2025-10-03 16:07:46 +0500

    adding vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,84 +4,86 @@ import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import { cn } from "@/lib/utils";
 import Footer from "@/components/Footer";
-  import { ToastContainer, Slide } from "react-toastify";
+import { ToastContainer, Slide } from "react-toastify";
+import { Analytics } from "@vercel/analytics/next";
 
-  const montserrat = Montserrat({
-    subsets: ["latin"],
-    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-    variable: "--font-montserrat",
-  });
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat",
+});
 
-  const poppins = Poppins({
-    variable: "--font-poppins",
-    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-    subsets: ["latin"],
-  });
+const poppins = Poppins({
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
-  export const metadata: Metadata = {
-    title: "Zainab Asif | Software Engineer",
-    description:
-      "Portfolio and projects of Zainab Asif, a full-stack developer specializing in Next.js, Tailwind, and modern web apps.",
-    icons: {
-      icon: "/ZainabAsif.svg",
-      apple: "/apple-touch-icon.png",
-    },
-    openGraph: {
-      title: "Zainab Asif | Full Stack Developer",
-      description: "Portfolio and projects of Zainab Asif",
-      url: "https://zainabasif.vercel.app",
-      siteName: "Zainab Asif",
-      images: [
-        {
-          url: "/website.png",
-          width: 1200,
-          height: 630,
-          alt: "Preview of Zainab's Portfolio",
-        },
-      ],
-      locale: "en_US",
-      type: "website",
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: "Zainab Asif | Full Stack Developer",
-      description: "Portfolio and projects of Zainab Asif",
-      images: ["/preview.png"],
-    },
-  };
+export const metadata: Metadata = {
+  title: "Zainab Asif | Software Engineer",
+  description:
+    "Portfolio and projects of Zainab Asif, a full-stack developer specializing in Next.js, Tailwind, and modern web apps.",
+  icons: {
+    icon: "/ZainabAsif.svg",
+    apple: "/apple-touch-icon.png",
+  },
+  openGraph: {
+    title: "Zainab Asif | Full Stack Developer",
+    description: "Portfolio and projects of Zainab Asif",
+    url: "https://zainabasif.vercel.app",
+    siteName: "Zainab Asif",
+    images: [
+      {
+        url: "/website.png",
+        width: 1200,
+        height: 630,
+        alt: "Preview of Zainab's Portfolio",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Zainab Asif | Full Stack Developer",
+    description: "Portfolio and projects of Zainab Asif",
+    images: ["/preview.png"],
+  },
+};
 
-  export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <html lang="en">
-        <body
-          className={cn(
-            `${poppins.variable} ${montserrat.variable} antialiased h-svh w-full flex flex-col max-w-[1750px] justify-self-center relative `
-          )}
-        >
-          <Navbar />
-          {/* Main content (scrollable + takes remaining height) */}
-          <main className="flex-1 mt-15 sm:mt-24 bg-black overflow-y-visible p-3">
-            {children}
-          </main>
-          <Footer />
-          <ToastContainer
-            position="bottom-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick={false}
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="dark"
-            transition={Slide}
-          />
-        </body>
-      </html>
-    );
-  }
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          `${poppins.variable} ${montserrat.variable} antialiased h-svh w-full flex flex-col max-w-[1750px] justify-self-center relative `
+        )}
+      >
+        <Navbar />
+        {/* Main content (scrollable + takes remaining height) */}
+        <main className="flex-1 mt-15 sm:mt-24 bg-black overflow-y-visible p-3">
+          {children}
+        </main>
+        <Footer />
+        <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+          transition={Slide}
+        />
+        <Analytics />
+      </body>
+    </html>
+  );
+}
